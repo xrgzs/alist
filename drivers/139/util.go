88d6e5bd8a0c6ad342019e71376863f8e3434f38
xrgzs <xiaoran@xrgzs.top>
2024-10-06 11:08:49 +0800

    fix(139): update family cloud API
    - Add new function familyGetLink to handle family cloud file links
    - Update Link function to use new familyGetLink for MetaFamily type
    - Modify familyGetFiles to include file path information
    - Update API endpoint for family cloud content query

--- drivers/139/util.go
+++ drivers/139/util.go
@@ -220,10 +220,11 @@ func (d *Yun139) familyGetFiles(catalogID string) ([]model.Obj, error) {
 			"sortDirection": 1,
 		})
 		var resp QueryContentListResp
-		_, err := d.post("/orchestration/familyCloud/content/v1.0/queryContentList", data, &resp)
+		_, err := d.post("/orchestration/familyCloud-rebuild/content/v1.2/queryContentList", data, &resp)
 		if err != nil {
 			return nil, err
 		}
+		path := resp.Data.Path
 		for _, catalog := range resp.Data.CloudCatalogList {
 			f := model.Object{
 				ID:       catalog.CatalogID,
@@ -243,6 +244,7 @@ func (d *Yun139) familyGetFiles(catalogID string) ([]model.Obj, error) {
 					Size:     content.ContentSize,
 					Modified: getTime(content.LastUpdateTime),
 					Ctime:    getTime(content.CreateTime),
+					Path:     path,
 				},
 				Thumbnail: model.Thumbnail{Thumbnail: content.ThumbnailURL},
 				//Thumbnail: content.BigthumbnailURL,
@@ -266,6 +268,18 @@ func (d *Yun139) getLink(contentId string) (string, error) {
 			"accountType": 1,
 		},
 	}
+	res, err := d.post("/orchestration/personalCloud/uploadAndDownload/v1.0/downloadRequest",
+		data, nil)
+	if err != nil {
+		return "", err
+	}
+	return jsoniter.Get(res, "data", "downloadURL").ToString(), nil
+}
+func (d *Yun139) familyGetLink(contentId string, path string) (string, error) {
+	data := d.newJson(base.Json{
+		"contentID": contentId,
+		"path":       path,
+	})
 	res, err := d.post("/orchestration/personalCloud/uploadAndDownload/v1.0/downloadRequest",
 		data, nil)
 	if err != nil {
